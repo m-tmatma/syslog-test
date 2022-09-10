cb93ccdb7629818f8553f80f2e01854002f9f97b
Masaru Tsuchiyama <m.tmatma@gmail.com>
2022-09-11 08:42:06 +0900

    add comment

--- syslog-test.c
+++ syslog-test.c
@@ -1,3 +1,33 @@
+/*!
+    @file
+
+    install developer tools.
+        $ sudo  apt install -y build-essential
+
+    compile
+        $ gcc -o syslog-test syslog-test.c
+
+    run
+        $ ./syslog-test
+
+    check syslog
+        $ hostname
+        ubuntu2204-01
+
+        $ cat /var/log/syslog
+        Sep 11 08:36:33 ubuntu2204-01 systemd[1592]: Started Application launched by gnome-session-binary.
+        Sep 11 08:36:56 ubuntu2204-01 syslog-test: [PID=3506] [PPID=2342] LOG_EMERG
+        Sep 11 08:36:56 ubuntu2204-01 syslog-test: [PID=3506] [PPID=2342] LOG_ALERT
+        Sep 11 08:36:56 ubuntu2204-01 syslog-test: [PID=3506] [PPID=2342] LOG_CRIT
+        Sep 11 08:36:56 ubuntu2204-01 syslog-test: [PID=3506] [PPID=2342] LOG_ERR
+        Sep 11 08:36:56 ubuntu2204-01 syslog-test: [PID=3506] [PPID=2342] LOG_WARNING
+        Sep 11 08:36:56 ubuntu2204-01 syslog-test: [PID=3506] [PPID=2342] LOG_NOTICE
+        Sep 11 08:36:56 ubuntu2204-01 syslog-test: [PID=3506] [PPID=2342] LOG_INFO
+        Sep 11 08:36:56 ubuntu2204-01 syslog-test: [PID=3506] [PPID=2342] LOG_DEBUG
+
+    If you received "Broadcast message from systemd-journald" on console running this program,
+    See https://github.com/boltgolt/howdy/issues/385#issuecomment-653710695
+*/
 #include <stdio.h>
 #include <sys/types.h>
 #include <unistd.h>
